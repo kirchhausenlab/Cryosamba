988f5fb5a1b13a3721d44fa67ba7959e301ef7f0
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-07-17 11:24:07 -0400

    adding logging config

---
+++ logging_config.py
@@ -0,0 +1,58 @@
+import logging
+import logging.config
+import sys
+from pathlib import Path
+
+# Set base directory for logs
+BASE_DIR = Path(__file__).resolve().parent
+LOGS_DIR=Path(BASE_DIR, "logs")
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+# making a logging config
+logging_config={
+    "version":1,
+    "disable_existing_loggers":False,
+    "formatters": {
+        "minimal":{"format":"%(message)s"},
+        "detailed":{
+            "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers":{
+        "console":{
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info":{
+            "class":"logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,
+            "backupCount": 10,
+            "formatter":"detailed",
+            "level": logging.INFO,
+        },
+        "error":{
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "root": {
+        "handlers": ["console", "info", "error"],
+        "level": logging.INFO,
+        "propagate": True,
+    },
+}
+logging.config.dictConfig(logging_config)
+logger=logging.getLogger()
+
+logger.debug("❗ For Debugging")
+logger.info("💻 Useful Messages from code ")
+logger.warning("⚠️S Something to be aware of")
+logger.error("💀 Mistake with the process")
+logger.critical("❌ critical error check")
