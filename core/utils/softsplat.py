fab517e38ba1b3a9baf5922b0a3caa443d041776
Miguel Esteva <esteva.m@wehi.edu.au>
2025-03-14 11:22:57 +1100

    Handling deprecation warnings amp.custom functions

--- core/utils/softsplat.py
+++ core/utils/softsplat.py
@@ -323,7 +323,7 @@ def softsplat(
 
 class _FunctionSoftsplat(torch.autograd.Function):
     @staticmethod
-    @torch.cuda.amp.custom_fwd(cast_inputs=torch.float32)
+    @torch.amp.custom_fwd(cast_inputs=torch.float32, device_type = 'cuda')
     def forward(self, tenIn, tenFlow):
         tenOut = tenIn.new_zeros(
             [tenIn.shape[0], tenIn.shape[1], tenIn.shape[2], tenIn.shape[3]]
@@ -414,7 +414,7 @@ class _FunctionSoftsplat(torch.autograd.Function):
     # end
 
     @staticmethod
-    @torch.cuda.amp.custom_bwd
+    @torch.amp.custom_bwd(device_type = 'cuda')
     def backward(self, tenOutgrad):
         tenIn, tenFlow = self.saved_tensors
 
